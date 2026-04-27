69ba19c43ed8fb3cc9a8a5efa8675c67890eee13
Antoine Maas <antoine.maas@okte.io>
2026-04-27 14:58:51 +0200

    test: cover missing Contact in complete_contact_details

--- erpnext/accounts/test_party.py
+++ erpnext/accounts/test_party.py
@@ -1,6 +1,6 @@
 import frappe
 
-from erpnext.accounts.party import get_default_price_list
+from erpnext.accounts.party import complete_contact_details, get_default_price_list
 from erpnext.tests.utils import ERPNextTestSuite
 
 
@@ -16,3 +16,22 @@ class PartyTestCase(ERPNextTestSuite):
 		customer.save()
 		price_list = get_default_price_list(customer)
 		assert price_list is None
+
+	def test_complete_contact_details_handles_missing_contact(self):
+		# Stale references to a non-existent Contact (e.g. set via direct DB write or
+		# a server script writing a display name into contact_person) used to crash
+		# with AttributeError: 'NoneType' object has no attribute 'items' because
+		# frappe.db.get_value returns None for an unknown name.
+		party_details = frappe._dict(
+			party_type="Customer",
+			party="_Test Customer",
+			contact_person="non-existent-contact-xyz",
+		)
+		complete_contact_details(party_details)
+		self.assertIsNone(party_details.contact_person)
+		self.assertIsNone(party_details.contact_display)
+		self.assertIsNone(party_details.contact_email)
+		self.assertIsNone(party_details.contact_mobile)
+		self.assertIsNone(party_details.contact_phone)
+		self.assertIsNone(party_details.contact_designation)
+		self.assertIsNone(party_details.contact_department)
